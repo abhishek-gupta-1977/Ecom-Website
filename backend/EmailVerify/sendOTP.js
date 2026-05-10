24d9f350a854c6705b3f32eaa4ea432842d6515a
abhishek--gupta <108aggupta@gmail.com>
2026-05-10 22:28:01 +0530

    Fix resend sender

--- backend/EmailVerify/sendOTP.js
+++ backend/EmailVerify/sendOTP.js
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 export const sendOTP = async (otp, email) => {
   try {
     const data = await resend.emails.send({
-      from: process.env.MAIL_USER,
+      from: "onboarding@resend.dev",
       to: email,
       subject: "Your OTP Verification Code",
       html: `<h1>Your OTP: ${otp}</h1>`,
